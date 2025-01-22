2eed492123bfb0e24941e46452ee517744cc113d
Alymbek <you@example.com>
2025-01-22 14:01:41 +0300

    First commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from "../../assets/HeaderAssets/images/Drile.svg"
+
+const Header = () => {
+    return (
+        <header>
+            <img src={ logo } alt="" />
+            <nav>
+                
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
